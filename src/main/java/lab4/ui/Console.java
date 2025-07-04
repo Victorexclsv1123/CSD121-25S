4e0974d81afaed20becadbee48a6fe873bcf9ba3
Victor Gian Lacuata <24045285@saultcollege.ca>
2025-07-04 00:15:44 -0400

    Lab-4-Donee

---
+++ src/main/java/lab4/ui/Console.java
@@ -0,0 +1,83 @@
+package lab4.ui;
+
+import lab4.game.*;
+
+import java.util.Scanner;
+
+import static com.diogonunes.jcolor.Ansi.colorize;
+import static com.diogonunes.jcolor.Attribute.*;
+
+/**
+ * Helper methods for doing console-based user interaction
+ */
+public class Console {
+
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    // Color-coded output helpers
+    private static String info(String msg)   { return colorize(msg, BLUE_TEXT()); }
+    private static String ok(String msg)     { return colorize(msg, GREEN_TEXT(), BOLD()); }
+    private static String warn(String msg)   { return colorize(msg, YELLOW_TEXT(), BOLD()); }
+    private static String err(String msg)    { return colorize(msg, RED_TEXT(), BOLD()); }
+
+    public static String prompt(String promptMessage) {
+        System.out.print(info(promptMessage));
+        var scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    /**
+     * Display the given game board
+     * @param board A tictactoe game board
+     */
+    public static void showBoard(Board board) {
+        System.out.print(board);
+    }
+
+    /**
+     * Repeatedly prompt the user for a position on which to place their next token.
+     * If they enter an invalid response or an already-taken position they are re-prompted.
+     * @param prompt The prompt to display to the user
+     * @param board The current state of the game board
+     * @return The position selected by the user
+     */
+    public static Position promptForPosition(String prompt, Board board) {
+
+        var scanner = new Scanner(System.in);
+        final String helpMessage = warn("Input must be in the format 'row column', e.g., '1 2' or 't m' for the top middle cell.");
+
+        while (true) {
+            System.out.print(info(prompt));
+            var input = scanner.nextLine().trim();
+
+            if (input.length() != 3) {
+                println(helpMessage);
+                continue;
+            }
+
+            var parts = input.split(" ");
+
+            if (parts.length != 2) {
+                println(helpMessage);
+                continue;
+            }
+
+            // The .from methods may throw if the user entered invalid location text, so we try/catch
+            try {
+                var pos = new Position(Row.from(parts[0]), Col.from(parts[1]));
+
+                if (board.isOccupiedAt(pos)) {
+                    println(err("That position is already taken."));
+                    continue;
+                }
+
+                return pos;
+
+            } catch (IllegalArgumentException e) {
+                println(err("Invalid input! " + helpMessage));
+            }
+        }
+    }
+}
